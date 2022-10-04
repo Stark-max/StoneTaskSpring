8d0cf51729f81bad2428f3f8cd37efcb0241980c
Kutman <kutman.usupov@gmail.com>
2022-10-04 17:25:36 +0600

    Finished

--- src/main/java/com/example/demostonetask/models/entities/Necklace.java
+++ src/main/java/com/example/demostonetask/models/entities/Necklace.java
@@ -3,17 +3,24 @@ package com.example.demostonetask.models.entities;
 import com.example.demostonetask.controllers.Display;
 import com.example.demostonetask.controllers.Factory;
 import com.example.demostonetask.controllers.Machine;
+import com.example.demostonetask.services.DisplayService;
+import com.example.demostonetask.services.FactoryService;
+import com.example.demostonetask.services.MachineService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Necklace {
     private ArrayList<Stone> stones = new ArrayList<>();
-    private Factory factory = new Factory();
-    private Display display = new Display();
-    private Machine machine = new Machine();
+    @Autowired
+    private FactoryService factory;
+    @Autowired
+    private DisplayService display;
+    @Autowired
+    private MachineService machine;
 
     public void product() {
         stones.addAll(factory.getNecklace());
@@ -46,7 +53,7 @@ public class Necklace {
     public void getStoneByColor(String color){
         display.displayStoneByColor(machine.findStoneByColor(color,stones));
     }
-   /* public void getAllInfo(){
+    public void getAllInfo(){
         display.representInfo(stones);
-    }*/
+    }
 }
--- src/main/java/com/example/demostonetask/services/FactoryService.java
+++ src/main/java/com/example/demostonetask/services/FactoryService.java
@@ -12,4 +12,5 @@ public interface FactoryService {
     double getCaratWeight(List<Stone> stones);
 
     int getCost(List<Stone> stones);
+
 }
